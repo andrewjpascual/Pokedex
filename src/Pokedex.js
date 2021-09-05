d6c14ea9bacf7450d782efe010d71cc416ed2e76
andrewjpascual <57194224+andrewjpascual@users.noreply.github.com>
2021-09-05 13:14:15 -0700

    Styling updates to front page

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -6,14 +6,17 @@ import {
   Card,
   CardMedia,
   CardContent,
+  CardHeader,
   CircularProgress,
   Typography,
   TextField,
 } from "@material-ui/core";
 import { makeStyles, alpha } from "@material-ui/core/styles";
+import CardActionArea from "@material-ui/core/CardActionArea";
 import SearchIcon from "@material-ui/icons/Search";
 import { toFirstCharUppercase } from "./constant";
 import axios from "axios";
+import logo from "./img/logo.png";
 
 const useStyles = makeStyles((theme) => ({
   pokedexContainer: {
@@ -21,26 +24,42 @@ const useStyles = makeStyles((theme) => ({
     paddingLeft: "50px",
     paddingRight: "50px",
   },
+  cardStyle: {
+    backgroundColor: "white",
+  },
   cardMedia: {
     margin: "auto",
+    width: "150px",
+    height: "150px",
+    backgroundColor: "#e6e6e6",
+    borderRadius: "50%",
   },
   cardContent: {
     textAlign: "center",
+  },
+  navStyle: {
+    backgroundColor: "#ff3333",
   },
   searchContainer: {
     display: "flex",
     backgroundColor: alpha(theme.palette.common.white, 0.15),
     paddingLeft: "20px",
     paddingRight: "20px",
-    margin: "auto",
+    marginTop: "5px",
+    marginBottom: "5px",
   },
   searchIcon: {
     alignSelf: "flex-end",
     marginBottom: "5px",
   },
   searchInput: {
-    width: "300px",
+    width: "200px",
     margin: "5px",
+  },
+  logo: {
+    maxWidth: "180px",
+    marginLeft: "25px",
+    marginRight: "25px",
   },
 }));
 
@@ -70,7 +89,7 @@ const Pokedex = (props) => {
           newPokemonData[index + 1] = {
             id: index + 1,
             name: pokemon.name,
-            sprite: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+            sprite: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${
               index + 1
             }.png`,
           };
@@ -79,43 +98,51 @@ const Pokedex = (props) => {
       });
   }, []);
 
+  //This deals with how each card looks
   const getPokemonCard = (pokemonId) => {
     const { id, name, sprite } = pokemonData[pokemonId];
 
     console.log(pokemonData[`${pokemonId}`]);
     return (
-      <Grid item xs={3} key={pokemonId}>
-        <Card onClick={() => history.push(`/${pokemonId}`)}>
-          <CardMedia
-            className={classes.cardMedia}
-            image={sprite}
-            style={{ width: "130px", height: "130px" }}
-          />
-          <CardContent className={classes.cardContent}>
-            <Typography>{`${id}. ${toFirstCharUppercase(name)}`}</Typography>
-          </CardContent>
+      <Grid item xs={4} key={pokemonId} className={classes.grid}>
+        <Card
+          onClick={() => history.push(`/${pokemonId}`)}
+          className={classes.cardStyle}
+          raised={true}
+        >
+          <CardActionArea>
+            <CardHeader variant="h3" title={`# ${id}`} />
+
+            <CardMedia className={classes.cardMedia} image={sprite} />
+            <CardContent className={classes.cardContent}>
+              <Typography variant="h4">{`${toFirstCharUppercase(
+                name
+              )}`}</Typography>
+            </CardContent>
+          </CardActionArea>
         </Card>
       </Grid>
     );
   };
 
-  //The pokemonData AKA "mockData" gets mapped into pokemonId, where getPokemonCard will use this data
+  //This deals with the layout of the main page
   return (
     <>
-      <AppBar position="static">
+      <AppBar position="static" className={classes.navStyle}>
         <Toolbar>
+          <img src={logo} alt="logo" className={classes.logo} />
           <div className={classes.searchContainer}>
             <SearchIcon className={classes.searchIcon} />
             <TextField
               onChange={handleSearchChange}
               className={classes.searchInput}
-              label="Search Pokemon, ID, Gen"
+              label="Search..."
             />
           </div>
         </Toolbar>
       </AppBar>
       {pokemonData ? (
-        <Grid container spacing={4} className={classes.pokedexContainer}>
+        <Grid container spacing={6} className={classes.pokedexContainer}>
           {Object.keys(pokemonData).map(
             (pokemonId) =>
               pokemonData[pokemonId].name.includes(filter) &&
