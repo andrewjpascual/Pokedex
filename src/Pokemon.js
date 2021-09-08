adf6eb4157a6acd4b32c3eec5a6ef982419abe7c
andrewjpascual <57194224+andrewjpascual@users.noreply.github.com>
2021-09-07 17:11:07 -0700

    Update Pokemon.js

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -1,13 +1,50 @@
 import React, { useState, useEffect } from "react";
-import { Typography, Link, CircularProgress, Button } from "@material-ui/core";
+import {
+  Typography,
+  Link,
+  CircularProgress,
+  Button,
+  Grid,
+  Card,
+  CardMedia,
+  CardContent,
+} from "@material-ui/core";
+import { makeStyles, alpha } from "@material-ui/core/styles";
 import { toFirstCharUppercase } from "./constant";
 import axios from "axios";
+import leftPad from "left-pad";
+import "@fontsource/alata";
+
+const newStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  Fullgrid: {
+    margin: "auto",
+    paddingTop: "200px",
+  },
+  cardStyle: {
+    marginTop: "80px",
+    marginLeft: "25%",
+
+    textAlign: "center",
+    width: "1000px",
+  },
+  backButton: {
+    marginTop: "30px",
+    marginLeft: "25%",
+    marginRight: "100px",
+    textAlign: "center",
+  },
+  cardBG: {},
+}));
 
 const Pokemon = (props) => {
   const { history, match } = props;
   const { params } = match;
   const { pokemonId } = params;
   const [pokemon, setPokemon] = useState(undefined);
+  const classes = newStyles();
 
   useEffect(() => {
     axios
@@ -25,30 +62,39 @@ const Pokemon = (props) => {
   //then use whatever variables necessary
   const generatePokemonJSX = () => {
     const { name, id, species, height, weight, types, sprites } = pokemon;
-    const fullImageUrl = `https://pokeres.bastionbot.org/images/pokemon/${id}.png`;
-    const { front_default } = sprites;
+
+    const fullPic = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`;
 
     return (
-      <>
-        <Typography variant="h1">
-          {`${id}.`}
-          {toFirstCharUppercase(name)}
-          <img src={front_default} />
-        </Typography>
-        <Typography variant="h3">Pokemon Info</Typography>
-        <Typography>
-          {"Species: "}
-          <Link href={species.url}>{species.name}</Link>
-        </Typography>
-        <Typography>Height : {height} </Typography>
-        <Typography>Weight : {weight} </Typography>
-        <Typography variant="h4">Types : </Typography>
-        {types.map((typeInfo) => {
-          const { type } = typeInfo;
-          const { name } = type;
-          return <Typography key={name}> {`${name}`}</Typography>;
-        })}
-      </>
+      <div className={classes.root}>
+        <Card className={classes.cardStyle} raised={true}>
+          <Grid container spacing={2} classname={classes.Fullgrid}>
+            <Grid item xs={6}>
+              <div className={classes.cardBG}>
+                <Typography variant="h1" className={classes.pokeID}>
+                  {` #${leftPad(id, 3, 0)}`}
+                </Typography>
+                <img src={fullPic}></img>
+                <Typography>{pokemon.types["0"].type.name}</Typography>
+              </div>
+              <Typography variant="h1">{toFirstCharUppercase(name)}</Typography>
+            </Grid>
+
+            <Grid item xs={6}>
+              <Typography variant="h3">Pokémon Info</Typography>
+              <Typography>Height : {height} </Typography>
+              <Typography>Weight : {weight} </Typography>
+              <Typography variant="h4">Types : </Typography>
+              <Typography>Types : </Typography>
+              {types.map((typeInfo) => {
+                const { type } = typeInfo;
+                const { name } = type;
+                return <li> {`${name}`}</li>;
+              })}
+            </Grid>
+          </Grid>
+        </Card>
+      </div>
     );
   };
 
@@ -60,11 +106,15 @@ const Pokemon = (props) => {
     <>
       {pokemon === undefined && <CircularProgress />}
       {pokemon !== undefined && pokemon && generatePokemonJSX()}
-      {pokemon === false && <Typography>Pokemon not found</Typography>}
+      {pokemon === false && <Typography>Pokémon not found</Typography>}
 
       {pokemon !== undefined && (
-        <Button variant="contained" onClick={() => history.push("/")}>
-          Back to pokedex
+        <Button
+          className={classes.backButton}
+          variant="contained"
+          onClick={() => history.push("/")}
+        >
+          Back to Pokédex
         </Button>
       )}
     </>
