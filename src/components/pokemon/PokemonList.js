5c4cc623270c6cf18e9a68d4ea1a1098e38f7972
andrewjpascual <57194224+andrewjpascual@users.noreply.github.com>
2021-09-17 15:36:20 -0700

    added categories by GEN

--- src/components/pokemon/PokemonList.js
+++ src/components/pokemon/PokemonList.js
@@ -8,9 +8,25 @@ import { FaGithub } from "react-icons/fa";
 export default class PokemonList extends Component {
   state = {
     url: "https://pokeapi.co/api/v2/pokemon?limit=151/",
+    url2: "https://pokeapi.co/api/v2/pokemon?limit=100&offset=151/",
+    url3: "https://pokeapi.co/api/v2/pokemon?limit=135&offset=251/",
+    url4: "https://pokeapi.co/api/v2/pokemon?limit=107&offset=386/",
+    url5: "https://pokeapi.co/api/v2/pokemon?limit=156&offset=493/",
+    url6: "https://pokeapi.co/api/v2/pokemon?limit=72&offset=649/",
+    url7: "https://pokeapi.co/api/v2/pokemon?limit=88&offset=721/",
+    url8: "https://pokeapi.co/api/v2/pokemon?limit=809/",
     pokemon: null,
     pokeTemp: null,
+    pokeTemp2: null,
+    pokeTemp3: null,
+    pokeTemp4: null,
+    pokeTemp5: null,
+    pokeTemp6: null,
+    pokeTemp7: null,
+    pokeTemp8: null,
+
     searchValue: "",
+    catValue: 1,
   };
 
   //information about pokemon
@@ -18,6 +34,27 @@ export default class PokemonList extends Component {
     const res = await axios.get(this.state.url);
     this.setState({ pokemon: res.data["results"] });
     this.setState({ pokeTemp: res.data["results"] });
+
+    const res2 = await axios.get(this.state.url2);
+    this.setState({ pokeTemp2: res2.data["results"] });
+
+    const res3 = await axios.get(this.state.url3);
+    this.setState({ pokeTemp3: res3.data["results"] });
+
+    const res4 = await axios.get(this.state.url4);
+    this.setState({ pokeTemp4: res4.data["results"] });
+
+    const res5 = await axios.get(this.state.url5);
+    this.setState({ pokeTemp5: res5.data["results"] });
+
+    const res6 = await axios.get(this.state.url6);
+    this.setState({ pokeTemp6: res6.data["results"] });
+
+    const res7 = await axios.get(this.state.url7);
+    this.setState({ pokeTemp7: res7.data["results"] });
+
+    const res8 = await axios.get(this.state.url8);
+    this.setState({ pokeTemp8: res8.data["results"] });
   }
 
   //user search for pokemon
@@ -29,12 +66,73 @@ export default class PokemonList extends Component {
 
   //calling this function will filter pokemon based on value
   filterPokemon = (userSearch) => {
-    const allPokemon = [...this.state.pokeTemp];
+    const allPokemon = [...this.state.pokeTemp8];
     this.setState({
       pokemon: allPokemon.filter((pokemon) =>
         pokemon.name.toLowerCase().includes(userSearch.toLowerCase())
       ),
     });
+  };
+
+  //category for pokemon
+  handleCatChange = (event) => {
+    const value = event.target.value;
+    this.setState({ catValue: value });
+    this.catOnPokemons(value);
+  };
+
+  //This will fix the category select
+  catOnPokemons = (gen) => {
+    const val = gen;
+    const allPokemon = [...this.state.pokeTemp];
+    const allPokemon2 = [...this.state.pokeTemp2];
+    const allPokemon3 = [...this.state.pokeTemp3];
+    const allPokemon4 = [...this.state.pokeTemp4];
+    const allPokemon5 = [...this.state.pokeTemp5];
+    const allPokemon6 = [...this.state.pokeTemp6];
+    const allPokemon7 = [...this.state.pokeTemp7];
+    const allPokemon8 = [...this.state.pokeTemp8];
+
+    if (val == 1) {
+      this.setState({
+        pokemon: allPokemon.filter((pokemon) => pokemon.name.toLowerCase()),
+      });
+    }
+    if (val == 2) {
+      this.setState({
+        pokemon: allPokemon2.filter((pokemon) => pokemon.name.toLowerCase()),
+      });
+    }
+    if (val == 3) {
+      this.setState({
+        pokemon: allPokemon3.filter((pokemon) => pokemon.name.toLowerCase()),
+      });
+    }
+    if (val == 4) {
+      this.setState({
+        pokemon: allPokemon4.filter((pokemon) => pokemon.name.toLowerCase()),
+      });
+    }
+    if (val == 5) {
+      this.setState({
+        pokemon: allPokemon5.filter((pokemon) => pokemon.name.toLowerCase()),
+      });
+    }
+    if (val == 6) {
+      this.setState({
+        pokemon: allPokemon6.filter((pokemon) => pokemon.name.toLowerCase()),
+      });
+    }
+    if (val == 7) {
+      this.setState({
+        pokemon: allPokemon7.filter((pokemon) => pokemon.name.toLowerCase()),
+      });
+    }
+    if (val == 8) {
+      this.setState({
+        pokemon: allPokemon8.filter((pokemon) => pokemon.name.toLowerCase()),
+      });
+    }
   };
 
   //maps the cards based on search
@@ -50,7 +148,22 @@ export default class PokemonList extends Component {
                 onChange={this.handleInputChange}
                 placeholder="Search for Pokémon..."
               />
+              <select
+                id="gen"
+                onChange={this.handleCatChange}
+                value={this.state.catValue}
+              >
+                <option value="1">GEN1</option>
+                <option value="2">GEN2</option>
+                <option value="3">GEN3</option>
+                <option value="4">GEN4</option>
+                <option value="5">GEN5</option>
+                <option value="6">GEN6</option>
+                <option value="7">GEN7</option>
+                <option value="8">ALL</option>
+              </select>
             </form>
+
             <div class="text-end">
               <a
                 href="http://github.com/andrewjpascual"
